0bb87b6fbfc25f10b36f20f5865a75a69b3d0a06
Patrik <patrik@Patrik.Patrik>
2025-05-21 21:38:33 +0000

    Use dynamic port for Render

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
-
 
